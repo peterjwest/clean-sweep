c188dcf6bb4a8f1ebaaeabee68619965152bb57f
Pete West <peterjwest3@gmail.com>
2023-08-16 14:57:12 +0100

    Fix parameter passing for CLI command

--- build/unlinted.sh
+++ build/unlinted.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 set -euo pipefail +x
-./node_modules/.bin/tsx ./node_modules/unlinted/build/code/unlinted.js
+./node_modules/.bin/tsx ./node_modules/unlinted/build/code/unlinted.js "$@"
